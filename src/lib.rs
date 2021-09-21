954c835978f3dcba29ce179f2bccf366b49c54a4
Donny Zimmanck <dzimmanck@enphaseenergy.com>
2021-09-21 10:56:39 -0700

    Changed trig library to use fixed-point types rather than primitive ints.  Dropped support for floating point, as the fixed-point library provides suficient abstraction and should have much higher performance in no_std CPUs without FPUs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
 pub mod trig;
-pub mod transforms;
+// pub mod transforms;
 pub mod common_tables;
+
+
+#[cfg(test)]
+mod tests {
+
+	// use fixed::types::extra::*;
+	// use fixed::FixedI16;
+	use fixed::types::I1F31;
+
+	#[test]
+    fn play_with_fixed() {
+
+    	let a = I1F31::from_num(0.768);
+    	let b = I1F31::from_num(0.56);
+    	let c = a.wide_mul(b);
+
+    	let y = I1F31::saturating_from_num(c);
+
+    	println!("{:?}", a);
+    	println!("{:?}", b);
+    	println!("{:?}", c);
+    	println!("{:?}", y);
+
+
+	}
+}
--- src/trig.rs
+++ src/trig.rs
@@ -1,13 +1,17 @@
 use crate::common_tables::SIN_TABLE;
+use fixed::types::I1F31;
 
 #[derive(Debug)]
-pub struct SinCos<T>{
-	pub sin: T,
-	pub cos:T
+pub struct SinCos{
+	pub sin: I1F31,
+	pub cos:I1F31
 }
 
 const DN: i32 = 0x1921FB5; // delta between the two points (fixed), in this case 2*pi/FAST_MATH_TABLE_SIZE
 const CONTROLLER_Q31_SHIFT: u32 = 32 - 9;
+
+const ZP500:I1F31 = I1F31::from_bits(0x4000_0000);
+const ZP866:I1F31 = I1F31::from_bits(0x6ed9_eba1);
 
 #[inline(always)]
 fn clip_i64_to_i32(x: i64) -> i32{
@@ -17,7 +21,7 @@ fn clip_i64_to_i32(x: i64) -> i32{
 }
 
 #[inline(always)]
-fn interpolate(f1: i32, f2: i32, d1: i32, d2: i32, fract: i32) -> i32{
+fn interpolate(f1: i32, f2: i32, d1: i32, d2: i32, fract: i32) -> I1F31{
 	let df = f2 - f1;
 	let mut temp: i64 = i64::from(DN)*(i64::from(d1) + i64::from(d2));
 	temp = temp - (i64::from(df) << 32);
@@ -26,22 +30,17 @@ fn interpolate(f1: i32, f2: i32, d1: i32, d2: i32, fract: i32) -> i32{
 	temp = i64::from(fract)*(temp >> 31);
 	temp = temp + i64::from(d1)*i64::from(DN);
 	temp = i64::from(fract)*(temp >> 31);
-	return clip_i64_to_i32((temp >> 31) + i64::from(f1));
+	I1F31::from_bits(clip_i64_to_i32((temp >> 31) + i64::from(f1)))
 }
 
-#[inline(always)]
-fn multiply(a: i32, b: i32) -> i64 {
-	i64::from(a) * i64::from(b)
-}
-
-impl SinCos <i32>{
-	pub fn from_theta(theta: i32) -> Self {
+impl SinCos {
+	pub fn from_theta(theta: I1F31) -> Self {
 		/* Calculate the nearest index */
-	  	let index_s:u16 = ((theta as u32) >> CONTROLLER_Q31_SHIFT) as u16;
+	  	let index_s:u16 = ((theta.to_bits() as u32) >> CONTROLLER_Q31_SHIFT) as u16;
 	  	let index_c:u16 = (index_s + 128) & 0x1ff;
 
 		/* Calculation of fractional value */
-	  	let fract: i32 = (theta - ((index_s as i32) << CONTROLLER_Q31_SHIFT)) << 8;
+	  	let fract: i32 = (theta.to_bits() - ((i32::from(index_s)) << CONTROLLER_Q31_SHIFT)) << 8;
 
 		/* Calculation of cosine value */
 		let mut f1: i32 = SIN_TABLE[(index_c+0) as usize] as i32;
@@ -65,13 +64,11 @@ impl SinCos <i32>{
 	/// Use Ptolemy's theorem rather than a new sin/cos lookup
 	pub fn shift_right_120(&self) -> Self {
 
-		let mut tmp: i64 = i64::from(self.sin)<<30;
-		tmp += multiply(self.cos, 1859775393);
-		let sin: i32 = (tmp >> 31) as i32;
+		let mut sin = self.sin * (-ZP500);
+		sin.saturating_mul_acc(ZP866, self.cos);
 
-		tmp = i64::from(self.cos)<<30;
-		tmp -= multiply(self.sin, 1859775393);
-		let cos: i32 = (tmp >> 31) as i32;
+		let mut cos = self.cos * (-ZP500);
+		cos.saturating_mul_acc(-ZP866, self.sin);
 
 		Self{sin, cos}
 	}
@@ -81,158 +78,88 @@ impl SinCos <i32>{
 	/// Use Ptolemy's theorem rather than a new sin/cos lookup
 	pub fn shift_left_120(&self) -> Self {
 
-		let mut tmp: i64 = i64::from(self.sin)<<30;
-		tmp -= multiply(self.cos, 1859775393);
-		let sin: i32 = (tmp >> 31) as i32;
+		let mut sin = self.sin * (-ZP500);
+		sin.saturating_mul_acc(-ZP866, self.cos);
 
-		tmp = i64::from(self.cos)<<30;
-		tmp += multiply(self.sin, 1859775393);
-		let cos: i32 = (tmp >> 31) as i32;
+		let mut cos = self.cos * (-ZP500);
+		cos.saturating_mul_acc(ZP866, self.sin);
 
 		Self{sin, cos}
 	}
 }
-
-impl SinCos <f32>{
-	pub fn from_theta(theta: f32) -> Self {
-		let sin_cos = theta.sin_cos();
-		Self{sin:sin_cos.0, cos:sin_cos.1}
-	}
-
-	/// Shifts sin/cos values 120 degrees right (+2pi/3)
-	///
-	/// Use Ptolemy's theorem rather than a new sin/cos lookup
-	pub fn shift_right_120(&self) -> Self {
-
-
-		let sin: f32 = self.sin * 0.5 + self.cos * 0.866025403784;
-		let cos: f32 = self.cos * 0.5 - self.sin * 0.866025403784;
-
-		Self{sin, cos}
-	}
-
-	/// Shifts sin/cos values 120 degrees left (-2pi/3)
-	///
-	/// Use Ptolemy's theorem rather than a new sin/cos lookup
-	pub fn shift_left_120(&self) -> Self {
-
-		let sin: f32 = self.sin * 0.5 - self.cos * 0.866025403784;
-		let cos: f32 = self.cos * 0.5 + self.sin * 0.866025403784;
-
-		Self{sin, cos}
-	}
-}
-
 
 #[cfg(test)]
-mod i32_tests {
+mod tests {
 
     use super::*;
 
 
     #[test]
     fn calc() {
-        let mut sin_cos: SinCos::<i32>;
+        let mut sin_cos: SinCos;
 
-        /* test at theta = -pi */
-        sin_cos = SinCos::<i32>::from_theta(-2147483648);
-        assert_eq!(sin_cos.sin, 0);
-        assert_eq!(sin_cos.cos, -2147483648);
+        // test at theta = -pi
+        sin_cos = SinCos::from_theta(I1F31::from_num(-1.));
+        assert_eq!(sin_cos.sin, I1F31::from_num(0.));
+        assert_eq!(sin_cos.cos, I1F31::from_num(-1.));
 
-        /* test at theta = -3*pi/4 */
-        sin_cos = SinCos::<i32>::from_theta(-1610612736);
-        assert_eq!(sin_cos.sin, -1518500250);
-        assert_eq!(sin_cos.cos, -1518500250);
+        // test at theta = -3*pi/4
+        sin_cos = SinCos::from_theta(I1F31::from_num(-3./4.));
+        assert_eq!(sin_cos.sin, I1F31::from_num(-0.707106781));
+        assert_eq!(sin_cos.cos, I1F31::from_num(-0.707106781));
 
-        /* test at theta = -pi/2 */
-        sin_cos = SinCos::<i32>::from_theta(-1073741824);
-        assert_eq!(sin_cos.sin, -2147483648);
-        assert_eq!(sin_cos.cos, 0);
+        // test at theta = -pi/2
+        sin_cos = SinCos::from_theta(I1F31::from_num(-1./2.));
+        assert_eq!(sin_cos.sin, I1F31::from_num(-1.));
+        assert_eq!(sin_cos.cos, I1F31::from_num(0.));
 
-        /* test at theta = -pi/4 */
-        sin_cos = SinCos::<i32>::from_theta(-536870912);
-        assert_eq!(sin_cos.sin, -1518500250);
-        assert_eq!(sin_cos.cos, 1518500250);
+        // test at theta = -pi/4
+        sin_cos = SinCos::from_theta(I1F31::from_num(-1./4.));
+        assert_eq!(sin_cos.sin, I1F31::from_num(-0.707106781));
+        assert_eq!(sin_cos.cos, I1F31::from_num(0.707106781));
 
-        /* test at theta = 0 */
-        sin_cos = SinCos::<i32>::from_theta(0);
-        assert_eq!(sin_cos.sin, 0);
-        assert_eq!(sin_cos.cos, 2147483647);
+        // test at theta = 0
+        sin_cos = SinCos::from_theta(I1F31::from_num(0.));
+        assert_eq!(sin_cos.sin, I1F31::from_num(0.));
+        assert_eq!(sin_cos.cos, I1F31::saturating_from_num(1.));
 
-        /* test at theta = pi/4 */
-        sin_cos = SinCos::<i32>::from_theta(536870912);
-        assert_eq!(sin_cos.sin, 1518500250);
-        assert_eq!(sin_cos.cos, 1518500250);
+        // test at theta = pi/4
+        sin_cos = SinCos::from_theta(I1F31::from_num(1./4.));
+        assert_eq!(sin_cos.sin, I1F31::from_num(0.707106781));
+        assert_eq!(sin_cos.cos, I1F31::from_num(0.707106781));
 
-        /* test at theta = pi/2 */
-        sin_cos = SinCos::<i32>::from_theta(1073741824);
-        assert_eq!(sin_cos.sin, 2147483647);
-        assert_eq!(sin_cos.cos, 0);
+        // test at theta = pi/2
+        sin_cos = SinCos::from_theta(I1F31::from_num(1./2.));
+        assert_eq!(sin_cos.sin, I1F31::saturating_from_num(1.));
+        assert_eq!(sin_cos.cos, I1F31::from_num(0.));
 
-        /* test at theta = 3*pi/4 */
-        sin_cos = SinCos::<i32>::from_theta(1610612736);
-        assert_eq!(sin_cos.sin, 1518500250);
-        assert_eq!(sin_cos.cos, -1518500250);
+        // test at theta = 3*pi/4
+        sin_cos = SinCos::from_theta(I1F31::from_num(3./4.));
+        assert_eq!(sin_cos.sin, I1F31::from_num(0.707106781));
+        assert_eq!(sin_cos.cos, I1F31::from_num(-0.707106781));
 
-        /* test at theta = pi */
-        sin_cos = SinCos::<i32>::from_theta(2147483647);
-        assert_eq!(sin_cos.sin, 2);
-        assert_eq!(sin_cos.cos, -2147483648);
+        // test at theta = pi
+        sin_cos = SinCos::from_theta(I1F31::wrapping_from_num(1.));
+        assert_eq!(sin_cos.sin, I1F31::from_num(0.));
+        assert_eq!(sin_cos.cos, I1F31::from_num(-1.));
     }
 
 
     #[test]
     fn shift_left() {
-        let sin_cos = SinCos::<i32>::from_theta(0);
+    	let angle = 0.2;
+        let sin_cos = SinCos::from_theta(I1F31::from_num(angle));
         let sin_cos_shift_left = sin_cos.shift_left_120();
-        assert_eq!(sin_cos_shift_left.sin, -1859775393);
-        assert_eq!(sin_cos_shift_left.cos, 1073741823);  // Should be 1073741824?
+        assert_eq!(sin_cos_shift_left.sin, I1F31::from_num(-0.994521895));
+        assert_eq!(sin_cos_shift_left.cos, I1F31::from_num(0.104528463));
     }
 
     #[test]
     fn shift_right() {
-        let sin_cos = SinCos::<i32>::from_theta(0);
+    	let angle = 0.2;
+        let sin_cos = SinCos::from_theta(I1F31::from_num(angle));
         let sin_cos_shift_right = sin_cos.shift_right_120();
-        assert_eq!(sin_cos_shift_right.sin, 1859775392);
-        assert_eq!(sin_cos_shift_right.cos, 1073741823);  // Should be 1073741824?
-    }
-}
-
-#[cfg(test)]
-mod f32_tests {
-
-    use super::*;
-
-    // define a simple macro for comparing floating point numbers
-    macro_rules! assert_delta {
-        ($x:expr, $y:expr, $d:expr) => {
-            if !($x - $y < $d || $y - $x < $d) { panic!(); }
-        }
-    }
-    
-
-
-    #[test]
-    fn calc() {
-        let sin_cos: SinCos::<f32>;
-        sin_cos = SinCos::<f32>::from_theta(2.356194490192345);
-        assert_delta!(sin_cos.sin, 0.70710677, 1e-5);
-        assert_delta!(sin_cos.cos, -0.70710677, 1e-5);
-    }
-
-    #[test]
-    fn shift_left() {
-        let sin_cos = SinCos::<f32>::from_theta(0.0);
-        let sin_cos_shift_left = sin_cos.shift_left_120();
-        assert_delta!(sin_cos_shift_left.sin, -0.8660254, 1e-10);
-        assert_delta!(sin_cos_shift_left.cos, 0.5, 1e-10);
-    }
-
-    #[test]
-    fn shift_right() {
-        let sin_cos = SinCos::<f32>::from_theta(0.0);
-        let sin_cos_shift_right = sin_cos.shift_right_120();
-        assert_delta!(sin_cos_shift_right.sin, 0.8660254, 1e-10);
-        assert_delta!(sin_cos_shift_right.cos, 0.5, 1e-10);
+        assert_eq!(sin_cos_shift_right.sin, I1F31::from_num(0.406736642));
+        assert_eq!(sin_cos_shift_right.cos, I1F31::from_num(-0.9135454576));
     }
 }
