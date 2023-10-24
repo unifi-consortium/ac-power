557e5d3d059806824dab6e5770b270ffd80bd983
Donny <dzimmanck@enphaseenergy.com>
2023-10-24 16:03:43 -0700

    Added telemetry.

--- src/plls/dsf.rs
+++ src/plls/dsf.rs
@@ -12,6 +12,22 @@ pub struct Dsf<const FRAC: i32> {
     pub cos: I1F31,
 }
 
+pub struct Telemetry {
+    pub theta: i32,
+    pub sin: i32,
+    pub cos: i32,
+    pub f: i32,
+
+    pub alpha: i32,
+    pub beta: i32,
+    pub zero: i32,
+
+    pub d_pos: i32,
+    pub q_pos: i32,
+    pub d_neg: i32,
+    pub q_neg: i32,
+}
+
 impl<const FRAC: i32> Dsf<FRAC> {
     pub fn new(fref: i32, kp: i32, ki: i32, max_integral: i32) -> Dsf<FRAC> {
         let fref = I1F31::from_bits(fref);
@@ -25,13 +41,13 @@ impl<const FRAC: i32> Dsf<FRAC> {
         }
     }
 
-    pub fn update(&mut self, abc: Abc<FRAC>) {
+    pub fn update(&mut self, abc: Abc<FRAC>) -> Telemetry {
         // clarke transform
         let alpha_beta = AlphaBeta::from(abc);
 
         // park transforms
         let dq0_pos = alpha_beta.to_dq0(self.sin, self.cos);
-        let _dq0_neg = alpha_beta.to_dq0(-self.sin, self.cos);
+        let dq0_neg = alpha_beta.to_dq0(-self.sin, self.cos);
 
         // TODO: Decoupling block
 
@@ -41,6 +57,21 @@ impl<const FRAC: i32> Dsf<FRAC> {
         // update the phase info
         self.theta = self.theta.wrapping_add(f);
         (self.sin, self.cos) = sin_cos(self.theta);
+
+        // return telemetry
+        Telemetry {
+            theta: self.theta.to_bits(),
+            sin: self.sin.to_bits(),
+            cos: self.cos.to_bits(),
+            f: f.to_bits(),
+            alpha: alpha_beta.alpha.to_bits(),
+            beta: alpha_beta.beta.to_bits(),
+            zero: alpha_beta.gamma.to_bits(),
+            d_pos: dq0_pos.d.to_bits(),
+            q_pos: dq0_pos.q.to_bits(),
+            d_neg: dq0_neg.d.to_bits(),
+            q_neg: dq0_neg.q.to_bits(),
+        }
     }
 }
 
