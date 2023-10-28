ae5c474545acc79c16413b0064b7ea98f3975ed2
Donny <dzimmanck@enphaseenergy.com>
2023-10-28 16:13:01 -0700

    Added lmt filter code.

--- src/plls/kalman.rs
+++ src/plls/kalman.rs
@@ -61,7 +61,8 @@ pub struct Kalman<const FRAC: i32> {
     pub theta: I0F32,
     sin: I1F31,
     cos: I1F31,
-    filter: PiFilter,
+    phase_filter: PiFilter,
+    clock_filter: PiFilter,
 }
 
 impl<const FRAC: i32> Kalman<FRAC> {
@@ -81,16 +82,23 @@ impl<const FRAC: i32> Kalman<FRAC> {
             .push(Term::new(7.0 * fref, ts, 0.03887693, -0.04452462))
             .unwrap();
 
-        // calculate the kp and ki of the PI controller
+        // calculate the gains of the phase filter
         let kp = 8.0 / (tset * vref);
         let ki = 16.0 / (gamma * gamma * tset * tset * vref);
+
+        // calculate the gains of clock filter
+        let ka = 50.0 * 2e3;
+        let k = 50.0;
 
         // normalize terms to the sampling frequency and conver to fixed-point
         let fref_norm = I0F32::from_num(fref * ts);
         let kp_norm = I0F32::from_num(kp * ts);
         let ki_norm = I0F32::from_num(ki * ts * ts);
+        let ka_norm = I0F32::from_num(ka * ts);
+        let k_norm = I0F32::from_num(k * ts * ts);
         let max_integral_norm = I0F64::from_num(max_integral * ts);
-        let filter = PiFilter::new(kp_norm, ki_norm, max_integral_norm);
+        let phase_filter = PiFilter::new(kp_norm, ki_norm, max_integral_norm);
+        let clock_filter = PiFilter::new(ka_norm, k_norm, max_integral_norm);
 
         Self {
             terms,
@@ -100,15 +108,16 @@ impl<const FRAC: i32> Kalman<FRAC> {
             cos: I1F31::MAX,
             fref: fref_norm,
             f: fref_norm,
-            filter,
+            phase_filter,
+            clock_filter,
         }
     }
-    pub fn update(&mut self, v: FixedI32<FRAC>) -> i32 {
+    pub fn update(&mut self, v: FixedI32<FRAC>) -> u32 {
         // kalman feedback section
-        let error = v - self.acc;
+        let v_error = v - self.acc;
         let mut acc = FixedI32::<FRAC>::ZERO;
         for term in &mut self.terms {
-            acc += term.update(error);
+            acc += term.update(v_error);
         }
         self.acc = acc;
 
@@ -116,18 +125,22 @@ impl<const FRAC: i32> Kalman<FRAC> {
         let dq = self.terms[0].alpha_beta.to_dq(self.sin, self.cos);
 
         // PI control loop
-        self.f = self.fref + self.filter.update(dq.q);
+        let f_error = self.phase_filter.update(dq.q);
+        self.f = self.fref + f_error;
 
         // update the phase info
         self.theta = self.theta.wrapping_add(self.f);
         (self.sin, self.cos) = sin_cos(self.theta);
 
+        // update the sample rate
+        let f_eff = self.fref + self.clock_filter.update(f_error);
+
         // calculate the next sample time
         // This division is expensive, but critical for accurate frequency measurement
         // TODO: Consider optimizing
-        let ratio: FixedI32<30> = self.fref.wide_div(self.f).cast();
+        let ratio: FixedI32<30> = f_eff.wide_div(self.f).cast();
         let mut lmt = FixedI32::<16>::from_bits(10_000 << 16);
         lmt *= ratio;
-        lmt.to_bits()
+        lmt.to_bits() as u32
     }
 }
