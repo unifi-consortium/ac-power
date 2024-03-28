f08c4a0ce7dc194a04b3b5ec6fa715f049056d6a
Donny <dzimmanck@enphaseenergy.com>
2024-03-28 15:28:34 -0700

    Code cleanup.  More doc.  More tests.

--- src/trig/mod.rs
+++ src/trig/mod.rs
@@ -13,6 +13,16 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
+/*!
+Trig functions and data-types.
+
+There are three types defined in the trig module
+
+1.  [Theta](crate::trig::types::Theta) - A representation of an angle between -pi and +pi radians (-180 and +180 degrees)
+2.  [Sin](crate::trig::types::Sin) - A representation of sin(theta).  Enforced to be between -1.0 and 1.0
+3.  [Cos](crate::trig::types::Cos) - A representation of cos(theta). Enforced to be between -1.0 and 1.0
+*/
+
 mod types;
 pub use types::{Cos, Sin, Theta};
 
@@ -38,7 +48,18 @@ pub fn cos_sin(theta: Theta) -> (Cos, Sin) {
     (cos.into(), sin.into())
 }
 
-// Use Ptolemy's theorem to rotate a vector
+/// Use Ptolemy's theorem to rotate a vector
+///
+/// # Examples
+///
+/// ```
+/// use ac_power::trig::{rotate, Theta, cos_sin};
+///
+/// let (x, y) = (1.0, 0.0);
+/// let theta = Theta::from_degrees(90.0);
+/// let (cos, sin) = cos_sin(theta);
+/// let (xr, yr) = rotate(x, y, cos, sin);
+/// ```
 pub fn rotate(x: f32, y: f32, cos: Cos, sin: Sin) -> (f32, f32) {
     let xr = x * cos - y * sin;
     let yr = x * sin + y * cos;
@@ -46,7 +67,18 @@ pub fn rotate(x: f32, y: f32, cos: Cos, sin: Sin) -> (f32, f32) {
     (xr, yr)
 }
 
-/// Shifts sin/cos values 120 degrees right (+2pi/3)
+/// Shifts a cos/sin pair 120 degrees right (+2pi/3)
+///
+/// # Examples
+///
+/// ```
+/// use ac_power::trig::{shift_right_120, Theta, cos_sin};
+///
+/// let (x, y) = (1.0, 0.0);
+/// let theta = Theta::from_degrees(90.0);
+/// let (cos, sin) = cos_sin(theta);
+/// let (cosr, sinr) = shift_right_120(cos, sin);
+/// ```
 pub fn shift_right_120(cos: Cos, sin: Sin) -> (Cos, Sin) {
     let (cosr, sinr) = rotate(
         cos.into(),
@@ -57,7 +89,18 @@ pub fn shift_right_120(cos: Cos, sin: Sin) -> (Cos, Sin) {
     (cosr.into(), sinr.into())
 }
 
-/// Shifts sin/cos values 120 degrees left (-2pi/3)
+/// Shifts a cos/sin pair 120 degrees left (-2pi/3)
+///
+/// # Examples
+///
+/// ```
+/// use ac_power::trig::{shift_left_120, Theta, cos_sin};
+///
+/// let (x, y) = (1.0, 0.0);
+/// let theta = Theta::from_degrees(90.0);
+/// let (cos, sin) = cos_sin(theta);
+/// let (cosr, sinr) = shift_left_120(cos, sin);
+/// ```
 pub fn shift_left_120(cos: Cos, sin: Sin) -> (Cos, Sin) {
     let (cosr, sinr) = rotate(
         cos.into(),
@@ -68,8 +111,21 @@ pub fn shift_left_120(cos: Cos, sin: Sin) -> (Cos, Sin) {
     (cosr.into(), sinr.into())
 }
 
-// use chebyshev method to calculate sin(Nx) and cos(Nx) from cos(x), sin((N-1)x), cos((N-1)x), sin((N-2)x), and cos((N-2)x)
-// https://trans4mind.com/personal_development/mathematics/trigonometry/multipleAnglesRecursiveFormula.htm
+/// Use [chebyshev method](https://trans4mind.com/personal_development/mathematics/trigonometry/multipleAnglesRecursiveFormula.htm) to calculate sin(Nx) and cos(Nx) from cos(x), sin((N-1)x), cos((N-1)x), sin((N-2)x), and cos((N-2)x)
+///
+/// # Examples
+///
+/// ```
+/// use ac_power::trig::{chebyshev, Theta, cos_sin, Sin, Cos};
+///
+/// let radians: f32 = 1.2;
+/// let theta = Theta::from_radians(radians);
+/// let (cos0, sin0) = (Cos::from(1.0), Sin::from(0.0));
+/// let (cos1, sin1) = cos_sin(theta);
+/// let (cos2, sin2) = chebyshev(cos1, sin1, cos1, sin0, cos0);
+/// let (cos3, sin3) = chebyshev(cos1, sin2, cos2, sin1, cos1);
+/// ```
+
 pub fn chebyshev(cos: Cos, sin1: Sin, cos1: Cos, sin2: Sin, cos2: Cos) -> (Cos, Sin) {
     let cosn = 2.0 * (cos * cos1) - cos2;
     let sinn = 2.0 * (cos * sin1) - sin2;
--- src/trig/types.rs
+++ src/trig/types.rs
@@ -2,16 +2,15 @@ use crate::constants::PI;
 use core::convert::From;
 use core::fmt;
 use core::ops::{Mul, MulAssign, Sub};
-use derive_more::From as dmFrom;
-use derive_more::Into as dmInto;
+use core::panic;
 
 #[derive(Copy, Clone, PartialEq)]
 pub struct Theta(pub i32);
 
-#[derive(Debug, Copy, Clone, PartialEq, dmFrom, dmInto)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Sin(f32);
 
-#[derive(Debug, Copy, Clone, PartialEq, dmFrom, dmInto)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Cos(f32);
 
 impl From<i32> for Sin {
@@ -108,6 +107,36 @@ impl MulAssign<Cos> for f32 {
     }
 }
 
+impl From<Sin> for f32 {
+    fn from(item: Sin) -> Self {
+        item.0
+    }
+}
+
+impl From<Cos> for f32 {
+    fn from(item: Cos) -> Self {
+        item.0
+    }
+}
+
+impl From<f32> for Sin {
+    fn from(item: f32) -> Self {
+        if item > 1.0 || item < -1.0 {
+            panic!("A Sin type must be between -1.0 and +1.0");
+        }
+        Self(item)
+    }
+}
+
+impl From<f32> for Cos {
+    fn from(item: f32) -> Self {
+        if item > 1.0 || item < -1.0 {
+            panic!("A Cos type must be between -1.0 and +1.0");
+        }
+        Self(item)
+    }
+}
+
 impl Theta {
     pub fn from_degrees(mut degrees: f32) -> Self {
         while degrees > 180.0 {
@@ -118,7 +147,7 @@ impl Theta {
         }
 
         let theta = ((degrees / 360.0) * (u32::MAX as f32)) as i32;
-        Self(theta)
+        theta.into()
     }
 
     pub fn to_degrees(&self) -> f32 {
@@ -134,12 +163,74 @@ impl Theta {
         }
 
         let theta = ((radians / (2.0 * PI)) * (u32::MAX as f32)) as i32;
-        Self(theta)
+        theta.into()
+    }
+
+    pub fn to_radians(&self) -> f32 {
+        (2.0 * PI) * (self.0 as f32) / (u32::MAX as f32)
+    }
+}
+
+impl From<i32> for Theta {
+    fn from(item: i32) -> Self {
+        Self(item)
+    }
+}
+
+impl From<Theta> for i32 {
+    fn from(item: Theta) -> Self {
+        item.0
     }
 }
 
 impl fmt::Debug for Theta {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "∠{}°", self.to_degrees())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn panics() {
+        let result = std::panic::catch_unwind(|| Sin::from(1.1));
+        assert!(result.is_err());
+
+        let result = std::panic::catch_unwind(|| Sin::from(-1.1));
+        assert!(result.is_err());
+
+        let result = std::panic::catch_unwind(|| Cos::from(1.1));
+        assert!(result.is_err());
+
+        let result = std::panic::catch_unwind(|| Cos::from(-1.1));
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn theta() {
+        let theta = Theta::from_degrees(90.0);
+        assert_eq!(i32::from(theta), 1073741824);
+
+        let theta = Theta::from_radians(0.5 * PI);
+        assert_eq!(i32::from(theta), 1073741824);
+    }
+
+    #[test]
+    fn multiplies() {
+        let cos = Cos::from(1.0);
+        let sin = Sin::from(1.0);
+
+        let x = 2.0;
+        assert_eq!(x * cos, x);
+        assert_eq!(x * sin, x);
+
+        let mut y = x;
+        y *= cos;
+        assert_eq!(x, y);
+        y *= sin;
+        assert_eq!(x, y);
     }
 }
